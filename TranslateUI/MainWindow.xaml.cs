c366da43d9fb86e10a32d46efb5bbc71a7eb7e9e
Luke Zhang <admin@lukezhang.win>
2024-12-06 11:39:43 +0800

    Add ComboBox UI

--- TranslateUI/MainWindow.xaml.cs
+++ TranslateUI/MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -12,6 +14,9 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Windows;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -21,6 +26,18 @@ namespace TranslateUI
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
+    /// 
+    public class Translation
+    {
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+    }
+
+    public class Root
+    {
+        [JsonPropertyName("translations")]
+        public Translation[] Translations { get; set; }
+    }
     public sealed partial class MainWindow : Window
     {
         public MainWindow()
@@ -35,6 +52,40 @@ namespace TranslateUI
         private void TranslateButton_Click(object sender, RoutedEventArgs e)
         {
             TranslateButton.Content = "Clicked";
+        }
+
+        private void CB_OutputLang_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CB_InputLang_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public string GetTranslatedText(string json)
+        {
+            try
+            {
+                var options = new System.Text.Json.JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+
+                var root = System.Text.Json.JsonSerializer.Deserialize<Root[]>(json, options);
+                if (root != null && root.Length > 0 && root[0].Translations.Length > 0)
+                {
+                    return root[0].Translations[0].Text;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions as needed
+                Debug.WriteLine($"Error parsing JSON: {ex.Message}");
+            }
+
+            return string.Empty;
         }
     }
 }
