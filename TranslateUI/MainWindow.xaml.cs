66c46a1b941e0f2ecfa5dff9e131f4712436b9eb
Luke Zhang <admin@lukezhang.win>
2024-12-06 23:10:59 +0800

    Prepare for main backend

--- TranslateUI/MainWindow.xaml.cs
+++ TranslateUI/MainWindow.xaml.cs
@@ -57,17 +57,13 @@ namespace TranslateUI
             string selectedOutputLang = CB_OutputLang.SelectedItem.ToString();
             string RealInputLang = "";
             string RealOutputLang = "";
-            string RealOnlyInputLang = "";
-            string RealOnlyOutputLang = "";
             if (selectedInputLang == "English")
             {
                 RealInputLang = "en-US";
-                RealOnlyInputLang = "en";
             }
             else if (selectedInputLang == "Chinsese (Simplified)")
             {
                 RealOutputLang = "zh-Hans";
-                RealOnlyOutputLang = "zh";
             }
             else
             {
@@ -114,11 +110,13 @@ namespace TranslateUI
             return string.Empty;
         }
 
-        private async void SendPostRequest()
+        private async void SendPostRequest(string TranslateToLang, string Content)
         {
             using (HttpClient client = new HttpClient())
             {
                 // 设置请求头
+                // 根据 @lingbopro 的研究，没什么用
+                // 但是还是设置一下
                 client.DefaultRequestHeaders.Add("sec-ch-ua-platform", "\"Windows\"");
                 client.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0");
                 client.DefaultRequestHeaders.Add("sec-ch-ua", "\"Microsoft Edge\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"");
@@ -130,19 +128,25 @@ namespace TranslateUI
                 client.DefaultRequestHeaders.Add("sec-mesh-client-os-version", "10.0.26100");
                 client.DefaultRequestHeaders.Add("sec-mesh-client-arch", "x86_64");
                 client.DefaultRequestHeaders.Add("sec-mesh-client-webview", "0");
+
+                // 瞎写的网站
                 client.DefaultRequestHeaders.Add("origin", "https://github.com");
                 client.DefaultRequestHeaders.Add("x-edge-shopping-flag", "1");
                 client.DefaultRequestHeaders.Add("sec-fetch-site", "cross-site");
                 client.DefaultRequestHeaders.Add("sec-fetch-mode", "cors");
                 client.DefaultRequestHeaders.Add("sec-fetch-dest", "empty");
-                client.DefaultRequestHeaders.Add("referer", "https://github.com/lingbopro/lingbos-sussy-mod/discussions/1");
+
+                // 瞎写的地址
+                client.DefaultRequestHeaders.Add("referer", "https://github.com/zsr-lukezhang/TranslateUI/");
                 client.DefaultRequestHeaders.Add("accept-encoding", "gzip, deflate, br, zstd");
-                client.DefaultRequestHeaders.Add("accept-language", "en,zh-CN;q=0.9,zh;q=0.8,en-GB;q=0.7,en-US;q=0.6");
+
+                // 这一行就不要了
+                // 省得影响输出内容
+                // client.DefaultRequestHeaders.Add("accept-language", "en,zh-CN;q=0.9,zh;q=0.8,en-GB;q=0.7,en-US;q=0.6");
                 client.DefaultRequestHeaders.Add("priority", "u=1");
 
                 // 请求Body
-                var content = new StringContent("{\"message\":\"Hi\"}", Encoding.UTF8, "application/json");
-
+                var content = new StringContent($"{{\"message\":\"{Content}\"}}", Encoding.UTF8, "application/json");
                 // 发送 POST 请求
                 HttpResponseMessage response = await client.PostAsync("https://https://edge.microsoft.com/translate/translatetext?from=&to=", content);
 
